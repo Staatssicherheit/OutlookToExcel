a417004186d8d0ce59135af13bdca033bd67f05b
Staatssicherheit <141138377+Staatssicherheit@users.noreply.github.com>
2024-04-23 22:42:51 +0900

    업데이트

---
+++ InstallSeleniumUsingCLI.ps1
@@ -0,0 +1,8 @@
+dotnet new console -n SeleniumAutomationProject -o /path/to/your/folder
+
+cd SeleniumAutomationProject 
+
+dotnet add package Selenium.WebDriver
+dotnet add package Selenium.Support
+dotnet add package Selenium.Chrome.WebDriver
+
